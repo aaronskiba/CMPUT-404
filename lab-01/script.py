cf980ffe4282a3b7c94c640ab1d1d8d9e8c7f473
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-09-15 19:33:21 -0600

    10. Modify your Python script so that it downloads itself from GitHub and prints out its own source code from GitHub.

--- lab-01/script.py
+++ lab-01/script.py
@@ -1,4 +1,6 @@
 import requests
 
 print("requests version = " + requests.__version__)
-print("GET http://www.google.com/ =", requests.get("http://www.google.com/"))
+print('requests.get("http://www.google.com/") =', requests.get("http://www.google.com/"))
+python_request = requests.get("https://raw.githubusercontent.com/aaronskiba/CMPUT-404/main/lab-01/script.py")
+print("python script:\n", python_request.text)
